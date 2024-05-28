510638fdda05a7e6d7a9796d51d1a85180acbd85
AndreyKuklinov <37548463+AndreyKuklinov@users.noreply.github.com>
2024-05-28 16:15:12 +0500

    Implement tooltip delay

--- Assets/Scripts/UI/Tooltips/TooltipManager.cs
+++ Assets/Scripts/UI/Tooltips/TooltipManager.cs
@@ -7,6 +7,9 @@ public class TooltipManager : MonoBehaviour
     public static TooltipManager Manager;
 
     [SerializeField] Tooltip _tooltip;
+    [SerializeField] float _delayBeforeShown;
+
+    private bool _isPointerOnTrigger;
 
     void Start()
     {
@@ -15,13 +18,22 @@ public class TooltipManager : MonoBehaviour
 
     public void Show(string header, string body)
     {
+        _isPointerOnTrigger = true;
         _tooltip.SetText(header, body);
         _tooltip.Update();
-        _tooltip.gameObject.SetActive(true);
+        StartCoroutine(ShowCoroutine());
     }
 
     public void Hide()
     {
+        _isPointerOnTrigger = false;
         _tooltip.gameObject.SetActive(false);
+    }
+
+    IEnumerator ShowCoroutine()
+    {
+        yield return new WaitForSeconds(_delayBeforeShown);
+        if(_isPointerOnTrigger)
+            _tooltip.gameObject.SetActive(true);
     }
 }
