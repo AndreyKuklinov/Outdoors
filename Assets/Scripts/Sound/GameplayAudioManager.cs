705a5d24905ea8d5bc6d0e38fb1990cce58c10a3
AndreyKuklinov <37548463+AndreyKuklinov@users.noreply.github.com>
2024-06-20 02:04:54 +0500

    Complete sounds

--- Assets/Scripts/Sound/GameplayAudioManager.cs
+++ Assets/Scripts/Sound/GameplayAudioManager.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 public class GameAudioManager : MonoBehaviour
 {
     [SerializeField] AudioClip _buildingPlacedClip;
+    [SerializeField] AudioClip _tileExploredClip;
     [SerializeField] GameBoard _gameBoard;
+    [SerializeField] TileExplorer _tileExplorer;
 
     void Start()
     {
         _gameBoard.BuildingPlaced += (int x, int y, BuildingType buildingType) 
             => PlayBuildingPlacedClip(); 
+
+        _tileExplorer.TileExplored += (TileType tileType) => PlayTileExploredClip();
     }
 
     void PlayBuildingPlacedClip()
     {
         SoundManager.Singleton.PlayClip(_buildingPlacedClip);
+    }
+
+    void PlayTileExploredClip()
+    {
+        SoundManager.Singleton.PlayClip(_tileExploredClip);
     }
 }
